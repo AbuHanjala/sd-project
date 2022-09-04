4cdf6228fa6b46003d8ef97129a1825015c1ff70
AbuHanjala <107362886+AbuHanjala@users.noreply.github.com>
2022-09-04 18:51:24 +0600

    Add files via upload

---
+++ sd project/Data Listing Page.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>aurnab restaurent</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link type="text/css" rel="stylesheet" href="/css/mycss.css">
+  </head>
+  <body>
+    <section>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Aurnab & Hanjala</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="HomePage.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="Data Listing Page.php">Foods</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="Contact Form Page.php">Contact</a>
+        </li>
+
+      </ul>
+    </div>
+  </div>
+</nav>
+    </section>
+    <section class="text-center bg-danger">
+      <h2>OUR ALL ITEMS</h2>
+    </section>
+
+
+    <section class="bg-dark p-2">
+      <div class="container">
+        <div class="row g-4">
+         <div class="col-md-4">
+           <div class="card bg-light">
+             <div class="card-body text-center">
+               <img
+                 src="./image/main_chicken_steak.png"
+                 class="rounded-circle mb-3" width="100%" height="100%"
+                 alt=""
+               />
+               <h3 class="card-title mb-3">CHICKEN STEAK</h3>
+               <p>Chicken Steak</p>
+               <p>BBQ Rice</p>
+               <p>Sauteed Mashroom</p>
+               <p>Sauteed Vegetable</p>
+               <p class="text-info"><b>487 taka</b></p>
+              <a href="#" class="btn btn-primary">Order Now</a>
+                </div>
+           </div>
+         </div>
+         <div class="col-md-4">
+           <div class="card bg-light">
+             <div class="card-body text-center">
+               <img
+                 src="./image/main_lemon_chicken.png"
+                 class="rounded-circle mb-3" width="100%" height="100%"
+                 alt=""
+               />
+               <h3 class="card-title mb-3">LEMON CHICKEN</h3>
+               <p>Lemon chicken</p>
+               <p>Mashed potato</p>
+               <p>Sliced salad</p>
+               <p>Wedges</p>
+               <p class="text-info"><b>554 taka</b></p>
+                 <a href="#" class="btn btn-primary">Order Now</a>
+                </div>
+           </div>
+         </div>
+         <div class="col-md-4">
+           <div class="card bg-light">
+             <div class="card-body text-center">
+               <img
+                 src="./image/main_lobster_thermidor.png"
+                 class="rounded-circle mb-3" width="100%" height="100%"
+                 alt=""
+               />
+               <h3 class="card-title mb-3">LOBSTER THERMIDOR</h3>
+               <p>Lobster</p>
+               <p>BBQ Rice</p>
+               <p>Wedges</p>
+               <p class="text-info"><b>2487 taka</b></p>
+                 <a href="#" class="btn btn-primary">Order Now</a>
+                </div>
+           </div>
+         </div>
+         <div class="col-md-4">
+           <div class="card bg-light">
+             <div class="card-body text-center">
+               <img
+                 src="./image/main_mongolian_chicken.png"
+                 class="rounded-circle mb-3" width="100%" height="100%"
+                 alt=""
+               />
+               <h3 class="card-title mb-3">MONGOLIAN CHICKEN STEAK</h3>
+               <p>Mongolian Chicken Steak</p>
+               <p>Masked Potato</p>
+               <p>Sauteed Vegetable</p>
+               <p class="text-info"><b>490 taka</b></p>
+                 <a href="#" class="btn btn-primary">Order Now</a>
+                </div>
+           </div>
+         </div>
+         <div class="col-md-4">
+           <div class="card bg-light">
+             <div class="card-body text-center">
+               <img
+                 src="./image/1280px-Hyderabadi_Chicken_Biryani.jpg"
+                 class="rounded-circle mb-3" width="100%" height="100%"
+                 alt=""
+               />
+               <h3 class="card-title mb-3">HYDERABADI CHICKEN BIRYANI</h3>
+               <p>Hyderabadi Chicken Biryani</p>
+               <p>Basmoti Rice</p>
+               <p>Chicken</p>
+               <p class="text-info"><b>200 taka</b></p>
+                 <a href="#" class="btn btn-primary">Order Now</a>
+                </div>
+           </div>
+         </div>
+         <div class="col-md-4">
+           <div class="card bg-light">
+             <div class="card-body text-center">
+               <img
+                 src="./image/Kacchi-Biryani-1-960x540.png"
+                 class="rounded-circle mb-3" width="100%" height="100%"
+                 alt=""
+               />
+               <h3 class="card-title mb-3">KACCHI BIRYANI</h3>
+               <p>Kacchi Biryani</p>
+               <p>Basmoti Rice</p>
+               <p>Boiled Egg</p>
+               <p>Salad</p>
+               <p class="text-info"><b>250 taka</b></p>
+                 <a href="#" class="btn btn-primary">Order Now</a>
+                </div>
+           </div>
+         </div>
+       </div>
+       <!-- <div class="col-md-4">
+         <div class="card bg-light">
+           <div class="card-body text-center">
+             <img
+               src="./image/beaf Burger.jpg"
+               class="rounded-circle mb-3" width="100%" height="100%"
+               alt=""
+             />
+             <h3 class="card-title mb-3">BEAF BURGER</h3>
+
+             <p class="text-info"><b>250 taka</b></p>
+               <a href="#" class="btn btn-primary">Order Now</a>
+              </div>
+         </div>
+       </div> -->
+       <!-- <div class="col-md-4">
+         <div class="bg-light">
+           <div class="text-center">
+             <img
+               src="./image/Pepperoni pizza.jpg"
+               class="rounded-circle mb-3" width="100%" height="100%"
+               alt=""
+             />
+             <h3 class="card-title mb-3">PEPPERONI PIZZA</h3>
+             <p>pizza dough</p>
+             <p>pizza sauce</p>
+             <p>pepperoni</p>
+             <p>mozzarella cheese</p>
+             <p>black pepper</p>
+             <p class="text-info"><b>460 taka</b></p>
+               <a href="#" class="btn btn-primary">Order Now</a>
+              </div>
+         </div>
+       </div> -->
+     </div>
+      </div>
+    </section>
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ sd project/categories.php
@@ -0,0 +1,201 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <!-- Important to make website responsive -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>online food ordaring  Website</title>
+
+    <!-- Link our CSS file -->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <!-- Navbar Section Starts Here -->
+    <section class="navbar">
+        <div class="container">
+            <div class="logo">
+                <a href="#" title="Logo">
+                    <img src="images/logo.png" alt="Restaurant Logo" class="img-responsive">
+                </a>
+            </div>
+
+            <div class="menu text-right">
+                <ul>
+                    <li>
+                        <a href="index.html">Home</a>
+                    </li>
+                    <li>
+                        <a href="categories.html">Categories</a>
+                    </li>
+                    <li>
+                        <a href="foods.html">Foods</a>
+                    </li>
+                    <li>
+                        <a href="Contact Form Page.HTML">Contact</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="clearfix"></div>
+        </div>
+    </section>
+    <!-- Navbar Section Ends Here -->
+
+
+
+    <!-- CAtegories Section Starts Here -->
+    <section class="categories">
+        <div class="container">
+            <h2 class="text-center">Explore Foods</h2>
+
+            <a href="category-foods.html">
+            <div class="box-3 float-container">
+                <img src="images/pizza.jpg" alt="Pizza" class="img-responsive img-curve">
+
+                <h3 class="float-text text-white">Pizza</h3>
+            </div>
+            </a>
+
+            <a href="#">
+            <div class="box-3 float-container">
+                <img src="images/burger.jpg" alt="Burger" class="img-responsive img-curve">
+
+                <h3 class="float-text text-white">Burger</h3>
+            </div>
+            </a>
+
+            <a href="#">
+            <div class="box-3 float-container">
+                <img src="images/momo.jpg" alt="Momo" class="img-responsive img-curve">
+
+                <h3 class="float-text text-white">Momo</h3>
+            </div>
+            </a>
+
+            <a href="#">
+            <div class="box-3 float-container">
+                <img src="image/drink_live_coconut.png" alt="coconut" class="img-responsive img-curve">
+
+                <h3 class="float-text text-white">coconut</h3>
+            </div>
+            </a>
+
+            <a href="#">
+            <div class="box-3 float-container">
+                <img src="image/drink_live_pineapple.png" alt="pineapple" class="img-responsive img-curve">
+
+                <h3 class="float-text text-white">pineapple</h3>
+            </div>
+            </a>
+
+            <a href="#">
+            <div class="box-3 float-container">
+                <img src="image/drink_fizzy_orange.png" alt="orange" class="img-responsive img-curve">
+
+                <h3 class="float-text text-white">orange</h3>
+            </div>
+            </a>
+            <a href="#">
+            <div class="box-3 float-container">
+                <img  src="images/main_mongolian_chicken.png" alt="CHICKEN STEAK" class="img-responsive img-curve">
+                <h3 class="float-text text-white">CHICKEN STEAK
+                <p>Chicken Steak</p>
+                <p>BBQ Rice</p>
+                <p>Sauteed Mashroom</p>
+                <p>Sauteed Vegetable</p></h3>
+            </div>
+            </a>
+
+            <a href="#">
+            <div class="box-3 float-container">
+                <img   src="./image/main_lemon_chicken.png" alt="LEMON CHICKEN" class="img-responsive img-curve">
+
+                <h3 class="float-text text-white">LEMON CHICKEN
+               <p>Lemon chicken</p>
+               <p>Mashed potato</p>
+               <p>Sliced salad</p></h3>
+
+            </div>
+            </a>
+
+            <a href="#">
+            <div class="box-3 float-container">
+                <img  src="./image/main_lobster_thermidor.png"alt="LOBSTER THERMIDOR" class="img-responsive img-curve">
+                <h3 class="float-text text-white">LOBSTER THERMIDOR
+                <p>Lobster</p>
+                <p>BBQ Rice</p></h3>
+            </div>
+            </a>
+
+            <a href="#">
+            <div class="box-3 float-container">
+                <img  src="images/main_mongolian_chicken.png" alt="Pizza" class="img-responsive img-curve">
+                <h3 class="float-text text-white">MONGOLIAN CHICKEN STEAK
+                <p>Mongolian Chicken Steak</p>
+                <p>Masked Potato</p>
+                <p>Sauteed Vegetable</p></h3>
+            </div>
+            </a>
+
+            <a href="#">
+            <div class="box-3 float-container">
+                <img src="images/menu-momo.jpg"alt="HYDERABADI CHICKEN BIRYANI" class="img-responsive img-curve">
+
+                <h3 class="float-text text-white">HYDERABADI CHICKEN BIRYANI
+                <p>Hyderabadi Chicken Biryani</p>
+                <p>Basmoti Rice</p>
+                <p>Chicken</p></h3>
+            </div>
+            </a>
+
+            <a href="#">
+            <div class="box-3 float-container">
+                <img src="images/Kacchi-Biryani-1-960x540.png" alt="KACCHI BIRYANI" class="img-responsive img-curve">
+
+             
+                <h3 class="float-text text-white">KACCHI BIRYANI
+                <p>Kacchi Biryani</p>
+                <p>Basmoti Rice</p>
+                <p>Boiled Egg</p>
+                <p>Salad</p>
+               </h3>
+            </div>
+            </a>
+
+            
+
+            <div class="clearfix"></div>
+        </div>
+    </section>
+    <!-- Categories Section Ends Here -->
+
+
+    <!-- social Section Starts Here -->
+    <section class="social">
+        <div class="container text-center">
+            <ul>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/50/000000/facebook-new.png"/></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/instagram-new.png"/></a>
+                </li>
+                <li>
+                    <a href="#"><img src="https://img.icons8.com/fluent/48/000000/twitter.png"/></a>
+                </li>
+            </ul>
+        </div>
+    </section>
+    <!-- social Section Ends Here -->
+
+    <!-- footer Section Starts Here -->
+    <section class="footer">
+        <div class="container text-center">
+            <p>Designed By <a href="#">HANJALA AND AURNOB</a></p>
+        </div>
+    </section>
+    <!-- footer Section Ends Here -->
+
+</body>
+</html>
